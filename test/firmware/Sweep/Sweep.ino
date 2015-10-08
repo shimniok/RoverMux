9e73a61a4d7192c006a1e349a7549b5209c52956
shimniok@gmail.com <shimniok@gmail.com>
2015-10-08 10:03:02 -0600

    Revised variable names, consolidated servoon/off into servoSet.

--- test/firmware/Sweep/Sweep.ino
+++ test/firmware/Sweep/Sweep.ino
@@ -1,17 +1,24 @@
 #include <TimerOne.h>
 #include <Servo.h> 
 
-#define CH1OUT  8
-#define CH1RC   9
-#define CH1MCU 10
+// Servo IDs
+#define RC1   0
+#define MCU1  1
+#define RC2   2
+#define MCU2  3
+#define RC3   4
+#define MCU3  5
 
+// Pins
 #define CH2OUT  5
 #define CH2RC   6
 #define CH2MCU  7
-
 #define CH3OUT  2
 #define CH3RC   3
 #define CH3MCU  4
+#define CH1OUT  8
+#define CH1RC   9
+#define CH1MCU 10
 
 #define PAUSE 200
 
@@ -27,25 +34,20 @@ Servo ch3;
  
 void setup() 
 {  
-  pinMode(CH1MCU, OUTPUT);
-  pinMode(CH1RC, OUTPUT);
   pinMode(CH1OUT, INPUT);
-
-  pinMode(CH2MCU, OUTPUT);
-  pinMode(CH2RC, OUTPUT);
   pinMode(CH2OUT, INPUT);
-
-  pinMode(CH3MCU, OUTPUT);
-  pinMode(CH3RC, OUTPUT);
   pinMode(CH3OUT, INPUT);
 
   Timer1.initialize(100);  // 100usec
-  Timer1.attachInterrupt(servos);
+  Timer1.attachInterrupt(servoHandler);
   Timer1.start();
-  
-  servoOn(0);
-  servoOn(3);
-  servoOn(4);
+
+  servoPin(RC1, CH1RC); 
+  servoPin(MCU1, CH1MCU);
+  servoPin(RC2, CH2RC);
+  servoPin(MCU2, CH2MCU);
+  servoPin(RC3, CH3RC);
+  servoPin(MCU3, CH3MCU); 
   
   Serial.begin(9600);
 } 
@@ -53,38 +55,45 @@ void setup()
  
 void loop() 
 { 
-
+  servoSet(RC1, 15);
+  delay(500);
 } 
 
 #define MAXSERVO 6
 long count=0;
 long period[MAXSERVO] = { 15, 15, 15, 15, 15, 15 };
-int rc[MAXSERVO] = { CH1RC, CH2RC, CH3RC, CH1MCU, CH2MCU, CH3MCU };
-int on[MAXSERVO] = { false, false, false, false, false, false };
+int pin[MAXSERVO] = { CH1RC, CH2RC, CH3RC, CH1MCU, CH2MCU, CH3MCU };
+int enabled[MAXSERVO] = { false, false, false, false, false, false };
 
 #define servoValid(s) (s >= 0 && s < MAXSERVO)
 
-void servoOn(int servo)
+/** note: not checking validity of pin */
+void servoPin(int servo, int p)
 {
-  if (servoValid(servo)) on[servo] = true;
-}
-
-void servoOff(int servo)
-{
-  if (servoValid(servo)) on[servo] = false;
-}
-
-/**
- * per -- usec length of signal, e.g., 1500 (1.5ms, typically center)
- */
-void servoSet(int servo, long per)
-{
-  if (servoValid(servo) && per >= 1000 && per <= 2000) {
-    period[servo] = per;
+  if (servoValid(servo)) {
+    pin[servo] = p;
+    pinMode(pin[servo], OUTPUT);
+    digitalWrite(pin[servo], LOW);
   }
 }
 
-void servos()
+
+/**
+ * per -- length of signal in 100's of usec, e.g., 15 for 1500usec (center)
+ */
+void servoSet(int servo, long per)
+{
+  if (servoValid(servo)) {
+    if (per == 0) {
+      enabled[servo] = false;
+    } else {
+      period[servo] = per;
+      enabled[servo] = true;
+    }
+  }
+}
+
+void servoHandler()
 {
   int i;
 
@@ -92,13 +101,13 @@ void servos()
 
   for (i = 0; i < MAXSERVO; i++) {
     if (count > period[i]) {
-      digitalWrite(rc[i], LOW);
+      digitalWrite(pin[i], LOW);
     }
   }
 
   if (count > 200) {
     for (i = 0; i < MAXSERVO; i++) {
-      digitalWrite(rc[i], on[i] ? HIGH : LOW);
+      digitalWrite(pin[i], enabled[i] ? HIGH : LOW);
     }
     count = 0;
   }
